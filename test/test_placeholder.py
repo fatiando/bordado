45401a4f74c41a1bee5c50d6baa7978f675ee0ec
Leonardo Uieda <leo@uieda.com>
2025-03-19 09:08:50 -0300

    Add a test so the CI doesn't fail

---
+++ test/test_placeholder.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2025 The Bordado Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
+"""
+A placeholder file until we have tests.
+"""
+import bordado
+
+
+def test_nothing():
+    assert bordado.__name__ == "bordado"
