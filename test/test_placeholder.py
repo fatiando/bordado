6d9cd76329bece14aade2f59afe1d74da7927619
Leonardo Uieda <leo@uieda.com>
2025-03-19 10:33:12 -0300

    Add GitHub Actions workflow for running tests (#3)
    Runs the tests and checks the coverage using a custom step instead of
    Codecov. Based on https://hynek.me/articles/ditch-codecov-python/. Add a
    dummy test so that `make test` doesn't fail because no tests were found.
    Install the package in editable mode instead of building a wheel because
    coverage can't find the source files otherwise. Coverage must be run
    with relative paths in order to combine the results from different OSs
    on CI.

---
+++ test/test_placeholder.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2025 The Bordado Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
+"""
+A placeholder file until we have tests.
+"""
+
+from bordado._dummy import dummy
+
+
+def test_nothing():
+    """Docstring."""
+    assert dummy() == "bla"
