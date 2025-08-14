50285c7f8b5be49e920292b46eb545dd3ed1984d
Leonardo Uieda <leo@uieda.com>
2025-08-14 08:22:57 -0300

    Add a function to generate random points on a sphere (#59)
    Generates uniform randomly distributed points on the surface of a
    sphere. Using the regular `random_coordinates` for this would lead to
    denser sampling at higher latitudes. To achieve this, we sample the
    cosine of latitude (in this case, colatitude) instead of the angles
    themselves. Test the code by calculating the point density per latitude
    band and making sure it's roughly uniform.

--- src/bordado/__init__.py
+++ src/bordado/__init__.py
@@ -12,7 +12,7 @@ from ._distance import neighbor_distance_statistics
 from ._grid import grid_coordinates
 from ._line import line_coordinates
 from ._profile import great_circle_coordinates, profile_coordinates
-from ._random import random_coordinates
+from ._random import random_coordinates, random_coordinates_spherical
 from ._region import get_region, inside, pad_region
 from ._split import (
     block_split,
--- src/bordado/_utils.py
+++ src/bordado/_utils.py
@@ -181,3 +181,62 @@ def shape_to_spacing(region, shape, *, pixel_register=False):
             n_points -= 1
         spacing.append((region[2 * i + 1] - region[2 * i]) / n_points)
     return tuple(reversed(spacing))
+
+
+def make_non_dimensional_coordinates(values, shape, dtype):
+    """
+    Make a list of arrays with the given values of non-dimensional coordinates.
+
+    These are coordinates that are not part of the dimensions of a grid or
+    spread of points. Think of them as extra coordinates.
+
+    Parameters
+    ----------
+    values : None or float or list or array
+        The values of the non-dimensional coordinates. If None, an empty list
+        will be returned. If a float, will generate one array of the desired
+        shape with this value as the fill. If a list or array, will generate
+        one array of the given shape per value in the list or array.
+    shape : tuple
+        The shape of the desired array(s).
+    dtype : str
+        A numpy compatible dtype used for the output array.
+
+    Returns
+    -------
+    coordinates : list
+        List of arrays generated. Even if a single value is passed, the output
+        will still be a list with a single array.
+
+    Examples
+    --------
+    Generating a single array:
+
+    >>> make_non_dimensional_coordinates(10, (2,), dtype="int")
+    [array([10, 10])]
+    >>> make_non_dimensional_coordinates(10, (2, 2), dtype="int")
+    [array([[10, 10],
+           [10, 10]])]
+
+    To generate multiple arrays:
+
+    >>> make_non_dimensional_coordinates([10, 20], (2,), dtype="int")
+    [array([10, 10]), array([20, 20])]
+    >>> import numpy as np
+    >>> make_non_dimensional_coordinates(np.array([10, 20]), (2,), dtype="int")
+    [array([10, 10]), array([20, 20])]
+    >>> make_non_dimensional_coordinates(
+    ...     np.array([[10], [20]]), (2,), dtype="int",
+    ... )
+    [array([10, 10]), array([20, 20])]
+
+    If None is given:
+
+    >>> make_non_dimensional_coordinates(None, (2,), dtype="int")
+    []
+    """
+    coordinates = []
+    if values is not None:
+        for value in np.atleast_1d(values).ravel():
+            coordinates.append(np.full(shape, value, dtype=dtype))
+    return coordinates
